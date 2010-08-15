e6833326790ba7fe01128352542b4704ea49a63d
Cem Eliguzel <cem@eliguzel.net>
2010-08-15 13:32:56 +0300

    * Background coolor is set to yellow. * A more meaningful message is displayed together with name of the store. * Css properties such as fontsize and alignment have been set.

--- chrome-extension/contentscript.js
+++ chrome-extension/contentscript.js
@@ -1,23 +1,44 @@
 
+function  createRootElement(id) {
+    root = document.createElement("div");
+    root.id = id;
+    root.style.backgroundColor = "#00FF00";
+    root.style.height = "25px";
+    root.style.fontFamily = "Trebuchet MS";
+    root.style.fontSize = "12px";
+    root.style.verticalAlign = "middle";
+    root.style.textAlign = "center";
+    document.body.insertBefore(root, document.body.firstChild);
+    return root;
+}
 
 function showBooks(responseText) {
   parser=new DOMParser();
   responseXML=parser.parseFromString(responseText,"text/xml");
   var books = responseXML.getElementsByTagName("book");
 
+//  alert(responseText);
+  notification = createRootElement("deneme");
   for (var i = 0, book; (book = books[i]) && i < 5; i++) {
     var a = document.createElement("a");
-    a.innerText = book.getAttribute("price") + ' TL, ' + book.getAttribute("name");
-    a.href = book.getAttribute("link");
-    document.body.appendChild(a);
-    document.body.appendChild(document.createElement("br"));
-    alert(a.innerText);
+    price = book.getAttribute("price");
+    name = book.getAttribute("name");
+    store = book.getAttribute("store");
+    link = book.getAttribute("link");
+    if (store == "1") {
+        storeName = "Imge.com.tr";
+    } else if (store == "2") {
+        storeName = "Idefix.com";
+    } else if (store == "3") {
+        storeName = "Kitapyurdu.com";
+    }
+    notification.innerHTML = "<p>Bu kitabı <b>" + price + "TL</b>'ye <b><a href='" + link + "'>" + storeName + "</a></b> sitesinden alabilirsiniz.</p>";
   }
   
 }
 
 var re = /ISBN.*[0-9]{10}/g;
 isbn = document.body.innerHTML.match(re)[0].slice(-10)
-alert(isbn);
+//alert(isbn);
 chrome.extension.sendRequest({'action' : 'fetchBooks', 'selectedText' : isbn}, showBooks);
 
