675676a8e960cf67d53098c2a4a074c63890be5c
Cem Eliguzel <cem@eliguzel.net>
2010-08-17 22:40:04 +0300

    Added pandora.com.tr spider

---
+++ crawler/crawler/spiders/pandora.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from crawler.items import BookItem
+from scrapy.contrib.loader import XPathItemLoader
+
+class PandoraSpider(CrawlSpider):
+    domain_name = 'pandora.com.tr'
+    start_urls = ['http://www.pandora.com.tr/']
+
+    rules = (
+        Rule(SgmlLinkExtractor(allow=(r'/urun\.aspx\?', )), 'parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+		l = XPathItemLoader(item=BookItem(), response=response)
+		l.add_xpath('name',     '//span[@id=\'ctl00_ContentPlaceHolderMainOrta_LabelAdi\']/text()')
+		l.add_xpath('isbn',     '//span[@id=\'ctl00_ContentPlaceHolderMainOrta_LabelIsbn\']/text()')
+		l.add_xpath('author',   '//span[@id=\'ctl00_ContentPlaceHolderMainOrta_LabelYazar\']/a/text()')
+		l.add_xpath('publisher','//a[@id=\'ctl00_ContentPlaceHolderMainOrta_HyperLinkYayinci\']/text()')
+		l.add_xpath('price',    '//span[@class=\'fiyat\']/text()', u'(.*) TL')
+		l.add_value('link', response.url)
+		l.add_value('store', 4)
+		return l.load_item()
+
+SPIDER = PandoraSpider()
