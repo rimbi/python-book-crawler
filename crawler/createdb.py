2f700aee4b005faacced3bcc2150359e4b51b5c3
cem <cem@cem-laptop.(none)>
2010-09-12 17:21:44 +0300

    Switched database from sqlite to mysql.
    The database name is bookcrawler.

--- crawler/createdb.py
+++ crawler/createdb.py
@@ -13,18 +13,18 @@ from sqlalchemy import Table, Column, Integer, Float, Unicode, MetaData, Foreign
 from sqlalchemy.orm import mapper, sessionmaker
 
 
-engine = create_engine('sqlite:///%s' % sys.argv[1], echo=True)
+engine = create_engine('mysql://root:123456@localhost/bookcrawler', echo=True)
 metadata = MetaData()
 
 books_table = Table('books', metadata,
 			Column('id', Integer, primary_key=True),
-			Column('isbn', Unicode),
-			Column('name', Unicode),
-			Column('author', Unicode),
-			Column('publisher', Unicode),
-			Column('link', Unicode),
-			Column('price', Float),
-			Column('store', Integer))
+			Column('isbn', Unicode(255)),
+			Column('name', Unicode(255)),
+			Column('author', Unicode(255)),
+			Column('publisher', Unicode(255)),
+			Column('link', Unicode(255)),
+			Column('price', Float(precision=2)),
+			Column('store', Integer)
+			)
 
 metadata.create_all(engine)
-
--- crawler/readdb.py
+++ crawler/readdb.py
@@ -13,6 +13,13 @@ from sqlalchemy import Table, Column, Integer, Float, Unicode, MetaData, Foreign
 from sqlalchemy.orm import mapper, sessionmaker
 from optparse import OptionParser
 
+parser=OptionParser()
+parser.add_option("-d", "--database", dest="database_name", help="select database file name", metavar="DATABASE")
+parser.add_option("-c", "--column", dest="column_name", help="select column name", metavar="COLUMN")
+parser.add_option("-q", "--query", dest="query_string", help="set query string", metavar="QUERY")
+(options, args) = parser.parse_args()
+
+
 class Book(object):
 	def __init__(self, name, isbn, author, publisher, link, price, store):
 		self.name = name
@@ -26,27 +33,21 @@ class Book(object):
 	def __repr__(self):
 		return u"<Book('%s', '%s', '%s', '%s', '%s', '%f' '%d')>" % (self.name, self.isbn, self.author, self.publisher, self.link, self.price, self.store)
 
-
-parser=OptionParser()
-parser.add_option("-d", "--database", dest="database_name", help="select database file name", metavar="DATABASE")
-parser.add_option("-c", "--column", dest="column_name", help="select column name", metavar="COLUMN")
-parser.add_option("-q", "--query", dest="query_string", help="set query string", metavar="QUERY")
-(options, args) = parser.parse_args()
-
-engine = create_engine('sqlite:///%s' % options.database_name, echo=True)
 metadata = MetaData()
 
 books_table = Table('books', metadata,
 			Column('id', Integer, primary_key=True),
-			Column('isbn', Unicode),
-			Column('name', Unicode),
-			Column('author', Unicode),
-			Column('publisher', Unicode),
-			Column('link', Unicode),
-			Column('price', Float),
+			Column('isbn', Unicode(255)),
+			Column('name', Unicode(255)),
+			Column('author', Unicode(255)),
+			Column('publisher', Unicode(255)),
+			Column('link', Unicode(255)),
+			Column('price', Float(precision=2)),
 			Column('store', Integer))
 
 mapper(Book, books_table)
+
+engine = create_engine('mysql://root:123456@localhost/%s' % options.database_name, echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
@@ -57,6 +58,7 @@ expressions = { "name"      : Book.name.like(u"%" + query + u"%"),
                 "isbn"      : Book.isbn == query,
               }
 
-for book in session.query(Book).filter(expressions[options.column_name]).order_by(Book.price):
+books = session.query(Book).filter(expressions[options.column_name]).order_by(Book.price)
+for book in books:
 	print "---------------------"
 	print "%s\n %s\n %s\n %s\n %s\n %f\n %d\n" % (book.name, book.author, book.publisher, book.link, book.isbn, book.price, book.store)
