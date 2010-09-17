42ebc293ccd847a8ae08636042fa3a3159c58a1b
Sinan Nalkaya <snalkaya@ubicom.com>
2010-09-17 15:31:40 +0300

    [Tools][createdb] * Added parameter support for followings;   - username (username of the database)   - password (password for the given/default username)   - host (host address of the database server)   - database (database name)   Since optparse is deprecated (version 2.7), argparse is used instead.
    * removed unnecessary library inclusion and comments

--- crawler/createdb.py
+++ crawler/createdb.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/topics/item-pipeline.html
-
-import sys
-import sqlalchemy
 
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, Float, Unicode, MetaData, ForeignKey, or_
-from sqlalchemy.orm import mapper, sessionmaker
+from sqlalchemy import Table, Column, Integer, Float, Unicode, MetaData
+from argparse import ArgumentParser
 
+parser = ArgumentParser(description='Allows to create initial books table in mysql database')
+parser.add_argument('-u', '--username', nargs=1, default=['root'], help='Username of the relevant database')
+parser.add_argument('-p', '--password', nargs=1, default=[''], help='Password for given or default username of the relevant database')
+parser.add_argument('--host', nargs=1, default=['localhost'], help='Host address of the database')
+parser.add_argument('-d', '--database', nargs=1, default=['bookcrawler'], help='Database name')
+options = parser.parse_args()
 
-engine = create_engine('mysql://root:123456@localhost/bookcrawler', echo=True)
+db_dialect = "mysql://" + options.username[0] + ":" + options.password[0] + "@" + options.host[0] + "/" + options.database[0]
+engine = create_engine(db_dialect, echo=True)
 metadata = MetaData()
 
 books_table = Table('books', metadata,
