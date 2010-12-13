5d1d06f2910533264c51741f92b7877694b22e5a
Sinan Nalkaya <snalkaya@ubicom.com>
2010-12-13 16:32:24 +0200

    [firefox-addon] * Added xml response parser. * Added notification to the extension (not finished).

--- firefox-addon/KitapSever/chrome/content/kitapsever.js
+++ firefox-addon/KitapSever/chrome/content/kitapsever.js
@@ -11,6 +11,42 @@ function HTMLParser(aHTMLString){
 	return body;
 };
 
+function notify(bookList) {
+	var message = "";
+	for (var i = 0, book; book = bookList[i]; i++) {
+		var bookStore = book.getAttribute("store");
+		var storeName;
+		var price = book.getAttribute("price");
+		if (bookStore == "1") {
+			storeName = "imge.com.tr";
+		} else if (bookStore == "2") {
+			storeName = "idefix.com";
+		} else if (bookStore == "3") {
+			storeName = "kitapyurdu.com";
+		} else if (bookStore == "4") {
+			storeName = "pandora.com.tr";
+		} else if (bookStore == "5") {
+			storeName = "netkitap.com";
+		} else if (bookStore == "6") {
+			storeName = "ilknokta.com";
+		}
+		if (i > 0) {
+			message = message + ", ";
+		} else if (i == bookList.length) {
+			message = message + " ";
+		}
+		message = message + storeName + " (" + price + " TL)";
+	}
+	if (bookList.length == 1) {
+		message = message + "sitesinden ";
+	} else {
+		message = message + "sitelerinden ";
+	}
+	message = message + "satin alabilirsiniz!";
+	var nb = gBrowser.getNotificationBox();
+	nb.appendNotification(message, 'kitapsever-notification');
+};
+
 var KitapSever = function () {
 	var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 	return {
@@ -33,15 +69,12 @@ var KitapSever = function () {
 
 			var req = new XMLHttpRequest();
 			req.open("GET", "http://127.0.0.1:8000/myapp/default/query.xml?column_name=isbn&query_string=" + bookIsbnNum, "true");
+			req.channel.loadFlags |= Components.interfaces.nsIRequest.LOAD_BYPASS_CACHE;
 			req.onreadystatechange = function (asyncEvent) {
 				if (req.readyState == 4) {
 					if (req.status == 200) {
-						alert("Incoming resp " + req.responseXML);
-						// var respBody = HTMLParser(req.responseText);
-						// var bookClass = respBody.getElementsByTagName("books");
-						// alert("book Name " + bookClass + ", length " + bookClass.length);
-//						var bookName = bookClass.
-
+						var books = req.responseXML.getElementsByTagName("book");
+						notify(books);
 					} else {
 						alert("Invalid response");
 					}
