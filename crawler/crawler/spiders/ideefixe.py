bdad785a8f9ad01f80803c84436e42d9b9500c38
Cem Eliguzel <cem@eliguzel.net>
2011-02-14 20:41:49 +0100

    Fix price extraction error in ideefix.com spider

--- crawler/crawler/spiders/ideefixe.py
+++ crawler/crawler/spiders/ideefixe.py
@@ -23,7 +23,7 @@ class IdefixSpider(CrawlSpider):
 		l.add_xpath('isbn',     '//div[@id=\'tanitimbox\']/text()', u'.*ISBN : ([0-9]+)')
 		l.add_xpath('author',   '//div[@class=\'boxTanimVideo\']/a/text()')
 		l.add_xpath('publisher','//h3[@class=\'boxTanimyayinevi\']/a/b/text()')
-		l.add_xpath('price',    '//b[@class=\'pricerange\']/text()', u'\s*(.*) TL \(KDV Dahil\)')
+		l.add_xpath('price',    '//b[@class=\'pricerange\']/text()', u'\s*([0-9,]*) TL \(KDV Dahil\)')
 		l.add_value('link', response.url)
 		l.add_value('store', 2)
 		return l.load_item()
