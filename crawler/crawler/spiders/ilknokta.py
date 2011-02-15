cc3e10154eaebbfa16243110a829aa98e2d2fc4e
Cem Eliguzel <cem@eliguzel.net>
2011-02-16 00:30:24 +0100

    Fixed ilknokta.com spider

--- crawler/crawler/spiders/ilknokta.py
+++ crawler/crawler/spiders/ilknokta.py
@@ -13,13 +13,13 @@ class IlknoktaSpider(CrawlSpider):
     start_urls = ['http://www.ilknokta.com/']
 
     rules = (
-        Rule(SgmlLinkExtractor(allow=(r'/urun/.*', ), unique=True), 'parse_item', follow=True),
+        Rule(SgmlLinkExtractor(allow=(r'/kitap/.*', ), unique=True), 'parse_item', follow=True),
         Rule(SgmlLinkExtractor(allow=(r'/.*', ), unique=True), ),
     )
 
     def parse_item(self, response):
 		l = XPathItemLoader(item=BookItem(), response=response)
-		l.add_xpath('name',     '//font[@class=\'baslikt\']/strong/text()')
+		l.add_xpath('name',     '//div[@class="divbaslik"]/@title')
 		l.add_xpath('isbn',     '//td/text()', u'.*ISBN: ([0-9\-]+)')
 		l.add_xpath('author',   '//td[@class=\'yazart\']/a/text()')
 		l.add_xpath('publisher','//a[@class=\'yayineviU\']/text()')
